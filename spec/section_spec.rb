e3f45f9f623b465c35ef4acf6233935a7a45a514
Ryan Veazey <ryan.veazey@gmail.com>
2014-05-19 23:22:14 -0500

    added more section specs

--- spec/section_spec.rb
+++ spec/section_spec.rb
@@ -1,33 +1,37 @@
 describe Section do
-  describe '#initialize' do
-    subject { Section.new data }
+  subject { section }
+  let(:section) { Section.new data }
+  let(:data) { [header, '1.2.3.4 domain'] }
+  let(:header) { "#section foo bar #{status}" }
+  let(:status) { 'ON' }
 
+  describe '#initialize' do
     context 'with a section' do
       let(:data) { [header, '1.2.3.4 domain'] }
-      let(:header) { '#section foo bar' }
+      let(:status) { '' }
 
       context 'without a space after #' do
         describe 'and no status' do
           it 'should parse the name' do
-            expect(subject.name).to eq 'foo bar'
+            expect(section.name).to eq 'foo bar'
           end
 
           it { should be_enabled }
         end
 
         describe 'and a status of ON' do
-          let(:header) { '#section foo bar ON' }
+          let(:status) { 'ON' }
           it 'should parse the name' do
-            expect(subject.name).to eq 'foo bar'
+            expect(section.name).to eq 'foo bar'
           end
 
           it { should be_enabled }
         end
 
         describe 'and a status of OFF' do
-          let(:header) { '#section foo bar OFF' }
+          let(:status) { 'OFF' }
           it 'should parse the name' do
-            expect(subject.name).to eq 'foo bar'
+            expect(section.name).to eq 'foo bar'
           end
 
           it { should_not be_enabled }
@@ -35,35 +39,36 @@ describe Section do
       end
 
       context 'with a space before and after #' do
+        let(:header) { " # section foo bar #{status}" }
+
         describe 'and no status' do
           it 'should parse the name' do
-            expect(subject.name).to eq 'foo bar'
+            expect(section.name).to eq 'foo bar'
           end
 
           it { should be_enabled }
         end
 
         describe 'and a status of ON' do
-          let(:header) { ' # section foo bar ON' }
+          let(:status) { 'ON' }
           it 'should parse the name' do
-            expect(subject.name).to eq 'foo bar'
+            expect(section.name).to eq 'foo bar'
           end
 
           it { should be_enabled }
         end
 
         describe 'and a status of OFF' do
-          let(:header) { ' # section foo bar OFF' }
+          let(:status) { 'OFF' }
           it 'should parse the name' do
-            expect(subject.name).to eq 'foo bar'
+            expect(section.name).to eq 'foo bar'
           end
-
           it { should_not be_enabled }
         end
       end
 
       it 'should not have the header in the data' do
-        expect(subject.data).to eq data.drop(1)
+        expect(section.data).to eq data.drop(1)
       end
     end
 
@@ -71,15 +76,28 @@ describe Section do
       let(:data) { ['1.2.3.4 domain'] }
       it { should be_enabled }
       it 'should not have a name' do
-        expect(subject.name).to be_nil
+        expect(section.name).to be_nil
       end
       it 'should not have a header' do
-        expect(subject.header).to be_nil
+        expect(section.header).to be_nil
       end
 
       it 'should not alter the data' do
-        expect(subject.data).to eq data
+        expect(section.data).to eq data
       end
     end
+  end
+
+  describe '#header' do
+    subject { section.header }
+    it { should end_with status  }
+    it { should start_with '#' }
+    it { should include section.name }
+  end
+
+  describe '#to_s' do
+    subject { section.to_s }
+    it { should start_with section.header }
+    it { should end_with section.data.last }
   end
 end
